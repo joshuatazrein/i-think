cf4c2b0e5360702c712a075a85908d04494d5609
Joshua Tazman Reinier <84872947+joshuatazrein@users.noreply.github.com>
2021-09-06 23:29:16 -0700

    fixed index links

--- assets/js/blogscript.js
+++ assets/js/blogscript.js
@@ -1,4 +1,22 @@
-$.get($('#indextext').attr('link'), 
-function (val, status, xhr) {
-  $('#indextext').html(xhr.responseText)
-})
+if ($('#indextext').attr('link') == 'index') {
+  console.log('yes');
+  for (fname of [
+    'artistic ideologies and practices',
+    'dharma',
+    'knowledge management'
+  ]) {
+    $.get(
+      './i/' + fname + '.html', 
+      function (val, status, xhr) {
+        console.log(xhr.responseText);
+        $('#indextext').html($('#indextext').html() + 
+        xhr.responseText.replace(/\.\//g, "./z/"))
+      }
+    )
+  }
+} else {
+  $.get($('#indextext').attr('link'), 
+  function (val, status, xhr) {
+    $('#indextext').html(xhr.responseText)
+  })
+}
