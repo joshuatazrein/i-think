8f10c7eb92a754552f2f63c034df4a0253025684
Joshua Tazman Reinier <84872947+joshuatazrein@users.noreply.github.com>
2021-10-07 01:27:20 -0400

    backlinksconsole

--- assets/js/blogscript.js
+++ assets/js/blogscript.js
@@ -1,25 +1,31 @@
-$.get(
-  'bookmarks.html',
-  function(message, status, xhr) {
-    $('#bookmarks').html(xhr.responseText);
-    setLinks('#bookmarks')
-    $('#bookmarks').append('<p class="buffer"></p>')
-  }
-);
+var index = {};
 
-$.get(
-  'bibliography.html',
-  function(message, status, xhr) {
-    $('#bibliography').html(xhr.responseText);
-    setLinks('#bibliography')
-  }
-);
+function updateLinks() {
+  $.get(
+    'bookmarks.html',
+    function(message, status, xhr) {
+      $('#bookmarks').html(xhr.responseText);
+      setLinks('#bookmarks');
+      $('#bookmarks').append('<p class="buffer"></p>');
+      updateBacklinks();
+    }
+  );
+  
+  $.get(
+    'bibliography.html',
+    function(message, status, xhr) {
+      $('#bibliography').html(xhr.responseText);
+      setLinks('#bibliography')
+    }
+  );
+}
 
 function setLinks(selector) {
   if (!selector) selector = ''
   $(selector + ' .link').on('mouseup', function (ev) {
     let el = $(ev.target)
-    while (el[0].tagName != 'SPAN') {
+    console.log(el);
+    while (!['SPAN', 'P'].includes(el[0].tagName)) {
       el = el.parent()
     }
     let link
@@ -31,10 +37,15 @@ function setLinks(selector) {
     $.get(
       'z/' + link + '.html',
       function (t, s, xhr) {
-        const splitlist = xhr.responseText.split('<hr>')
-        $('#zettels').html(splitlist[0]);
-        if (splitlist[1]) {
-          $('#backlinks').html(splitlist[1]);
+        $('#zettels').html('<h4>' + link + '</h4>' + 
+          xhr.responseText);
+        if (index[link]) {
+          $('#backlinks').html(
+            // list of backlinks
+            '<p class="link">' + 
+            index[link].join('</p><p class="link">') + 
+            '</p>');
+          console.log($('#backlinks').html());
         }
         setLinks('#container')
         $('#zettels').scrollTop(0)
@@ -45,13 +56,21 @@ function setLinks(selector) {
         $('html, body').animate({scrollTop: $('#header').height() }, 500)
         $('.currentlink').removeClass('currentlink')
         child.addClass('currentlink')
+        const prevlink = $('#history').find('p.link').toArray().find(x => {
+          return $(x).text() == link
+        })
+        if (prevlink) prevlink.remove();
+        $('#history').html('<p class="link">' + link + '</p>' + 
+          $('#history').html());
+        setLinks('#history');
+        setLinks('#backlinks');
       }
     );
   });
   $(selector + ' .b-link').on('mouseup', function (ev) {
     let link
     let el = $(ev.target)
-    while (el[0].tagName != 'SPAN') {
+    while (!['SPAN', 'P'].includes(el[0].tagName)) {
       el = el.parent()
     }
     if (el.attr('href')) {
@@ -63,10 +82,15 @@ function setLinks(selector) {
     $.get(
       'b/' + link + '.html',
       function (t, s, xhr) {
-        const splitlist = xhr.responseText.split('<hr>')
-        $('#zettels').html(splitlist[0]);
-        if (splitlist[1]) {
-          $('#backlinks').html(splitlist[1]);
+        $('#zettels').html('<h4>' + link + '</h4>' + 
+          xhr.responseText);
+        if (index[link]) {
+          $('#backlinks').html(
+            // list of backlinks
+            '<p class="link">' + 
+            index[link].join('</p><p class="link">') + 
+            '</p>');
+          console.log($('#backlinks').html());
         }
         setLinks('#container')
         $('#zettels').scrollTop(0)
@@ -77,7 +101,45 @@ function setLinks(selector) {
         $('html, body').animate({scrollTop: $('#header').height() }, 500)
         $('.currentlink').removeClass('currentlink')
         child.addClass('currentlink')
+        const prevlink = $('#history').find('p.link').toArray().find(x => {
+          return $(x).text() == link
+        })
+        if (prevlink) prevlink.remove();
+        $('#history').html('<p class="link">' + link + '</p>' + 
+          $('#history').html());
+        setLinks('#history');
+        setLinks('#backlinks');
       }
     );
   });
 }
+
+function updateBacklinks() {
+  // indexes backlinks
+  index = {}
+  $('#bookmarks .link').toArray().forEach(x => {
+    const title = $(x).text();
+    console.log('./z/' + title + '.html');
+    $.get('./z/' + title + '.html', 
+    function (s, m, xhr) {
+      // find all backlinks
+      $('#test').html(xhr.responseText);
+      console.log($('#test').html());
+      $('#test').find('.link').toArray().forEach(y => {
+        // add link text
+        const el = $(y);
+        console.log(el);
+        let text;
+        if (el.attr('href')) { text = el.attr('href') }
+        else { text = el.text() }
+        // add to dict: [article], [backlinks]
+        console.log(text, index[text]);
+        if (!index[text]) { index[text] = [title]}
+        else { index[text].push(title) }
+      })
+    })
+  })
+  setTimeout(function () { console.log(index); }, 1000);
+}
+
+updateLinks();
